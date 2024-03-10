c0e21e165ab6375a3c470a4051dec6cdddfb7d76
szczek <ignacy.rolinski@gmail.com>
2024-03-10 09:47:53 +0100

    styles(Tables): enhance + unify styling + fixes

---
+++ sprawdzFodmap/src/Components/Nav_Tables.tsx
@@ -0,0 +1,107 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { grey } from '@mui/material/colors';
+import { Link, Menu, MenuItem, Fade, IconButton } from '@mui/material';
+
+import { useState } from 'react';
+
+import FoodBankIcon from '@mui/icons-material/FoodBank';
+
+
+
+
+export default function ButtonAppBar({ open, handleDrawerOpen }: any) {
+
+  
+
+  const [anchorEl1, setAnchorEl1] = useState<null | HTMLElement>(null);
+  const openDropdown1 = Boolean(anchorEl1);
+  const handleClickDropdown1 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleCloseDropdown1= () => {
+    setAnchorEl1(null);
+  };
+
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+  const openDropdown2 = Boolean(anchorEl2);
+  const handleClickDropdown2 = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl2(event.currentTarget);
+  };
+  const handleCloseDropdown2= () => {
+    setAnchorEl2(null);
+  };
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
+        <Toolbar>
+          <Link className="page-brand" font-weight="800" href="/">
+                SprawdźFODMAP
+          </Link>
+          <IconButton className="drawer-button"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            >
+                <FoodBankIcon />
+            </IconButton>
+          <div className="nav-buttons-div">
+              <Button
+                id="fade-button"
+                aria-controls={openDropdown1 ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={openDropdown1 ? 'true' : undefined}
+                style={{color: grey[500] }}
+                onClick={handleClickDropdown1}
+              >
+                Tabele
+              </Button>
+              <Menu
+                id="fade-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'fade-button',
+                }}
+                anchorEl={anchorEl1}
+                open={openDropdown1}
+                onClose={handleCloseDropdown1}
+                TransitionComponent={Fade}
+              >
+                <MenuItem onClick={handleCloseDropdown1} component='a' href='/tabele/fodmap'>Fodmap</MenuItem>
+                <MenuItem onClick={handleCloseDropdown1} component='a' href='/tabele/hit'>Nietolerancja Histaminy</MenuItem>
+              </Menu>
+              <Button
+                id="fade-button"
+                aria-controls={openDropdown2 ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={openDropdown2 ? 'true' : undefined}
+                style={{color: grey[500] }}
+                onClick={handleClickDropdown2}
+              >
+                Info
+              </Button>
+              <Menu
+                id="fade-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'fade-button',
+                }}
+                anchorEl={anchorEl2}
+                open={openDropdown2}
+                onClose={handleCloseDropdown2}
+                TransitionComponent={Fade}
+              >
+                <MenuItem onClick={handleCloseDropdown2} component='a' href='/info/fodmap'>Dieta FODMAP</MenuItem>
+                <MenuItem onClick={handleCloseDropdown2} component='a' href='/info/hit'>Nietolerancja histaminy</MenuItem>
+
+              </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- sprawdzFodmap/src/Pages/HIT_Chart.tsx
+++ sprawdzFodmap/src/Pages/HIT_Chart.tsx
@@ -1,16 +1,130 @@
+// import './Tables.css';
+
+// import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+// import { Accordion, AccordionDetails, AccordionSummary, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+// import Navbar from '../Components/Nav.tsx'
+
+// import products from '../../public/db/dietDb.ts'
+
+// import { ProductProp } from './Home.tsx';
+// import { red } from '@mui/material/colors';
+
+
+// const getHistamineTier = function (tier: string) {
+
+//     return products.filter(
+//          (p) => p.histamine === tier)
+//          .sort((a, b) => {
+//              if (a.name < b.name)
+//              return - 1;
+//              if (a.name > b.name)
+//              return 1;
+ 
+//              return 0;
+//          }
+//      )
+//  }
+
+//  const lowTier = getHistamineTier('20%');
+//  const midTier = getHistamineTier('20-60%');
+//  const highTier = getHistamineTier('>60%');
+
+//  const sortedByHistamine = [ lowTier, midTier, highTier ];
+
+//  console.log(sortedByHistamine)
+//  export default function HIT_Chart() {
+//     return(
+//         <>
+//             <Navbar />
+
+           
+//             <Grid container spacing={2}>
+//                 <div className="button-scroll">
+//                         <Button href="#table-cat-20%">20%</Button>
+//                         <Button href="#table-cat-20-60%">20-60%</Button>
+//                         <Button href="#table-cat->60%">60%</Button>
+
+//                         <Button disabled ></Button>
+//                         <Button href="/tabele/fodmap" style={{color: red[400] }}>Tabela FODMAP</Button>
+//                 </div>
+
+//                 <Grid xs={12} md={5} className="table-grid">
+//                 <h2>Tabela diet przeciwhistaminowych</h2>
+//                 <Accordion className="table-accordion">
+//                     <AccordionSummary
+//                     expandIcon={<ExpandMoreIcon />}
+//                     aria-controls="panel1-content"
+//                     id="panel1-header"
+//                     >
+//                     O co z tym chodzi?
+//                     </AccordionSummary>
+//                     <AccordionDetails>
+//                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+//                     malesuada lacus ex, sit amet blandit leo lobortis eget.
+//                     </AccordionDetails>
+//                 </Accordion>                    {sortedByHistamine.map((group, i) => ( 
+//                         <div id={`table-cat-${group[0].histamine}`} className ="category-table" key={i}>
+//                             <h1 >{group[0].histamine}</h1>
+//                             <TableContainer component={Paper}>
+//                                 <Table sx={{ minWidth: 250 }} aria-label="simple table">
+//                                     <TableHead>
+//                                             <TableRow>
+//                                                 <TableCell>Produkt</TableCell>
+//                                                 <TableCell>Kategoria FODMAP</TableCell>
+//                                             </TableRow>
+//                                         </TableHead>
+//                                     <TableBody>
+//                                     {group.map((p: ProductProp) => (
+//                                         <TableRow
+//                                         key={p.id}
+//                                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//                                         >
+//                                             <TableCell component="th" scope="row">
+//                                                 {p.name} <br />
+//                                                 {p.sub_title}
+//                                             </TableCell>
+//                                             <TableCell>
+//                                                 {p.fodmap}
+//                                             </TableCell>
+//                                         </TableRow>
+//                                     ))}
+//                                     </TableBody>
+//                                 </Table>
+//                             </TableContainer>
+//                         </div>
+//                     ))}
+//                 </Grid>
+//             </Grid>
+//         </>
+//     )
+// }
+
+
+
 import './Tables.css';
 
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Accordion, AccordionDetails, AccordionSummary, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import Navbar from '../Components/Nav.tsx'
+import Navbar from '../Components/Nav_Tables.tsx'
 
 import products from '../../public/db/dietDb.ts'
 
 import { ProductProp } from './Home.tsx';
 import { red } from '@mui/material/colors';
 
+import { useState } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const drawerWidth = 240;
 
 const getHistamineTier = function (tier: string) {
 
@@ -33,54 +147,104 @@ const getHistamineTier = function (tier: string) {
 
  const sortedByHistamine = [ lowTier, midTier, highTier ];
 
- console.log(sortedByHistamine)
- export default function HIT_Chart() {
-    return(
-        <>
-            <Navbar />
 
-           
-            <Grid container spacing={2}>
-                <div className="button-scroll">
-                        <Button href="#table-cat-20%">20%</Button>
-                        <Button href="#table-cat-20-60%">20-60%</Button>
-                        <Button href="#table-cat->60%">60%</Button>
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
-                        <Button disabled ></Button>
-                        <Button href="/tabele/fodmap" style={{color: red[400] }}>Tabela FODMAP</Button>
+export default function HitTable() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    < >
+
+        <Navbar  open={ open } handleDrawerOpen = { handleDrawerOpen }/>
+        <Grid container spacing={2}>
+
+             <div className="button-scroll button-scroll-widescreen">
+                         <Button href="#table-cat-20%">🟡 20%</Button>
+                         <Button href="#table-cat-20-60%">🟠 20-60%</Button>
+                         <Button href="#table-cat->60%">🔴 60%</Button>
+
+                         <Button disabled ></Button>
+                         <Button href="/tabele/fodmap" style={{color: red[400] }}>Tabela FODMAP</Button>
                 </div>
 
-                <Grid xs={12} md={5} className="table-grid">
-                <h2>Tabela diet przeciwhistaminowych</h2>
-                <Accordion className="table-accordion">
-                    <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                    >
-                    O co z tym chodzi?
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </AccordionDetails>
-                </Accordion>                    {sortedByHistamine.map((group, i) => ( 
-                        <div id={`table-cat-${group[0].histamine}`} className ="category-table" key={i}>
-                            <h1 >{group[0].histamine}</h1>
-                            <TableContainer component={Paper}>
-                                <Table sx={{ minWidth: 250 }} aria-label="simple table">
-                                    <TableHead>
-                                            <TableRow>
-                                                <TableCell>Produkt</TableCell>
-                                                <TableCell>Kategoria FODMAP</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                    <TableBody>
-                                    {group.map((p: ProductProp) => (
-                                        <TableRow
+        <Drawer
+            sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+            },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+        >
+            <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+            </DrawerHeader>
+            <Divider />
+                <div className="button-scroll">
+                         <Button href="#table-cat-20%">20%</Button>
+                         <Button href="#table-cat-20-60%">20-60%</Button>
+                         <Button href="#table-cat->60%">60%</Button>
+                         <Button disabled ></Button>
+                         <Button href="/tabele/fodmap" style={{color: red[400] }}>Tabela FODMAP</Button>
+               </div>
+            <Divider />
+        </Drawer>
+
+        <Grid xs={12} md={5} className="table-grid">
+                         <h2>Tabela diet przeciwhistaminowych</h2>
+                        <Accordion className="table-accordion">
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                            >
+                            O co z tym chodzi?
+                            </AccordionSummary>
+                            <AccordionDetails>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            </AccordionDetails>
+                        </Accordion>
+
+                        {sortedByHistamine.map((group, i) => ( 
+                            <div id={`table-cat-${group[0].histamine}`} className ="category-table" key={i}>
+                                     <h1 >{group[0].histamine}</h1>
+                                     <TableContainer component={Paper}>
+                                         <Table sx={{ minWidth: 250 }} aria-label="simple table">
+                                             <TableHead>
+                                                     <TableRow>
+                                                         <TableCell>Produkt</TableCell>
+                                                         <TableCell>Kategoria FODMAP</TableCell>
+                                                     </TableRow>
+                                                 </TableHead>
+                                             <TableBody>
+                                        {group.map((p: ProductProp) => (
+                                     <TableRow
                                         key={p.id}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                             <TableCell component="th" scope="row">
                                                 {p.name} <br />
                                                 {p.sub_title}
@@ -89,14 +253,14 @@ const getHistamineTier = function (tier: string) {
                                                 {p.fodmap}
                                             </TableCell>
                                         </TableRow>
-                                    ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </div>
-                    ))}
-                </Grid>
+                                        ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
+                        ))}
+                    </Grid>
             </Grid>
-        </>
-    )
+    </ >
+  );
 }
