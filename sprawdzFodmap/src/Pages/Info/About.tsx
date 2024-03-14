47772fd00601f51bdbe12c5cf263b455b31384d0
szczek <ignacy.rolinski@gmail.com>
2024-03-14 21:11:21 +0100

    feat: add about section page + content and style it

--- sprawdzFodmap/src/Pages/Info/About.tsx
+++ sprawdzFodmap/src/Pages/Info/About.tsx
@@ -1,35 +1,71 @@
 
-import { Grid } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import Navbar from '../../Components/Nav.tsx'
 import './About.css'
+import Carousel from "react-material-ui-carousel";
+
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+
+const Slide_1 = function () {
+    return(
+        <div className="info-slide">
+            <h2 className="info-header">Kim jesteśmy?</h2>
+                <ul className="slide-text about-us-text"> SprawdźFODMAP to charytatywny projekt edukacyjny zrodzony z: 
+                <li>zamiłowania do tworzenia </li>  <li>wiary w prawo każdego do dostępu dobrej jakości informacji</li> <li> głębokiej pogardy wobec bólu brzucha </li>
+                </ul>   
+        </ div>
+    )
+}
+
+const Slide_2 = function () {
+    return(
+        <div className="info-slide">
+            <h2 className="info-header">Nasza misja</h2>
+            <ul className="slide-text">
+                <li>Inicjatywa ta nie ma charakteru komercyjnego.</li>
+                <li>Mamy nadzieję ułatwić Ci życie - pozwolić zarządzać swoją dietą zgodnie z aktualną wiedzą naukową.</li> 
+            </ul>
+        </ div>
+    )
+}
+
+const Slide_3 = function () {
+    return(
+        <div className="info-slide">
+            <h2 className="info-header">Kontakt</h2>
+            <ul className="slide-text">
+                <li>
+                    Masz jakieś uwagi? Chcesz zgłosić błąd?
+                </li>
+                <li>
+                    A może chcesz się odezwać i rozwijać ten (jeszcze) skromny projekt razem z nami?
+                </li>
+                <li>
+                Pisz do nas pod adres: <a className="email-link" href="mailto:sprawdzfodmap@gmail.com">sprawdzfodmap@gmail.com</a>
+                </li>
+            </ul>
+        </ div>
+    )
+}
+
 
 export default function Info () {
 
-
     return(
-        <>
+        <div className='about-page'>
             <Navbar />
             <Grid container spacing={2}>
                 <Grid xs={12} md={10} className="diet-grid">
-                    <h1>O nas</h1>
-
-                    <div>
-                        <p>
-                            SprawdzFODMAP to charytatywny projekt edukacyjny zrodzony z pasji tworzenia, bólu brzucha i wiary w prawo każdego człowieka do dostępu dobrej jakości informacji. 
-                            <br />
-                            Inicjatywa ta nie ma i nie będzie mieć charakteru komercyjnego. Mamy nadzieję pomóc Ci, kiedy masz dość googlowania - przy zakupach, przy gotowaniu i w chwili zwątpienia. 
-                            <br />
-                            Możesz się z nami skontaktować pod adresem: <a className="email-link" href="mailto:sprawdzfodmap@gmail.com">sprawdzfodmap@gmail.com</a>
-
-
-                        </p>
-
-                    </div>
-
-
+                    {/* <h1>O nas</h1> */}
+                    <Carousel height={500} animation={'slide'} duration={1600} swipe={true} cycleNavigation={false} PrevIcon={<ArrowBackOutlinedIcon />} NextIcon={<ArrowForwardOutlinedIcon />}	  >
+                        <Slide_1 />
+                        <Slide_2 />
+                        <Slide_3 />
+                    </ Carousel>
                 </Grid>
             </Grid>
                     
-        </>
-)
+        </ div>
+    )
 }
