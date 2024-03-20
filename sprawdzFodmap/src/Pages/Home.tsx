655499df73803584eed14c0aa144e78773beac1e
szczek <ignacy.rolinski@gmail.com>
2024-03-20 11:20:03 +0100

    feat(main): add modal w/ safety disclaimer;

--- sprawdzFodmap/src/Pages/Home.tsx
+++ sprawdzFodmap/src/Pages/Home.tsx
@@ -1,7 +1,7 @@
-import {CircularProgress, TextField } from "@mui/material";
+import {Box, Button, Checkbox, CircularProgress, FormControlLabel, Modal, TextField } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './Home.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import products from '../../public/db/dietDb.ts'
 import DietCard from "../Components/Main/DietCard.tsx";
 import Navbar from '../Components/Nav.tsx'
@@ -57,8 +57,63 @@ function search(arr: Array<ProductProp>, q: string){
 
     }    
 
+
+      const [openModal, setOpenModal] = useState(false);
+      const handleOpenModal = () => setOpenModal(true);
+      
+      const handleCloseModal = () => {
+        setOpenModal(false);
+        localStorage.setItem('disclaimer-seen', 'true');
+      }
+
+
+      const [disableModalButton, setDisableModalButton] = useState(true)
+
+      useEffect(() => {
+        if(localStorage.getItem('disclaimer-seen') === null) {
+            setOpenModal(true);
+        }
+      })
+
     return(
+
         <div className="main-page">
+
+            <Button onClick={handleOpenModal}>Open modal</Button>
+            <Modal
+            open={openModal}
+            aria-labelledby=""
+            aria-describedby=""
+            >
+                <Box className="first-time-modal" >
+                    <h2 id="disclaimer-modal-title" className="header">
+                        Dobrze Cię widzieć!
+                    </h2>
+                    <h5 className="header">
+                        Jednak zanim przejdziesz dalej, zapoznaj się, proszę, z poniższym zastrzeżeniem
+                    </h5>
+                    <ul id="disclaimer-modal-description">
+                        <li className="li-indent">
+                            Niniejsza strona zawiera informacje na temat stosowania diet, oparte na aktualnych źródłach naukowych i przeznaczona jest celom edukacyjnym. 
+                            
+                        </li>
+                        <li className="li-indent">
+                            Wszelkie kwestia diagnostyczne oraz decyzje dotyczące stosowania interwencji dietetycznych należy podejmować <b>jedynie zgodnie z zaleceniami lekarza i/lub dietetyka kliniczego</b>. 
+
+                        </li>
+                        <li className="li-indent">
+
+                        </li>
+                        <li className="li-indent">
+                        </li>
+                    </ul>
+                    <FormControlLabel required control={<Checkbox onClick={() => setDisableModalButton(!disableModalButton)} />} label="Potwierdzam zapoznanie się z powyższymi informacjami" />
+                    <Button disabled={disableModalButton} onClick={handleCloseModal}>Idź dalej</Button>
+                </Box>
+            </Modal>
+
+
+
             <Navbar />
             <Grid container spacing={2} className="home-grid">
                 <Grid xs={12} md={10} className="diet-grid">
