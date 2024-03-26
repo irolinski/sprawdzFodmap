19447597234069647b019f4e41bad4d554946652
szczek <ignacy.rolinski@gmail.com>
2024-03-26 10:29:14 +0100

    fix: set the data (vite config & package.json) for deploying on own domain; fix the router showing blank page on load

--- sprawdzFodmap/src/main.tsx
+++ sprawdzFodmap/src/main.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter  basename={import.meta.env.DEV ? '/' : '/wrazliwe-jelita/'}>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
