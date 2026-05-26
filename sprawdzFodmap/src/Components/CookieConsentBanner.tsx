bafa1b9e9e53b4f1bf48c6054d54f7f159fd62bb
irolinski <ignacy.rolinski@gmail.com>
2026-05-26 15:15:03 +0200

    feat: add CookieConsentBanner, handle cookie consent

---
+++ sprawdzFodmap/src/Components/CookieConsentBanner.tsx
@@ -0,0 +1,44 @@
+import { CookieConsent } from "cookie-consent-react";
+import { loadGoogleAnalytics } from "./../analytics/google-analytics.ts";
+import { useEffect, useState } from "react";
+
+const COOKIE_CONSENT_STORAGE_KEY = "sprawdzfodmap_cookie_consent";
+
+export default function CookieConsentBanner(disclaimerModalState: {
+  disclaimerModalState: boolean;
+}) {
+  const [cookieConsentBannerIsOpened, setCookieConsentBannerIsOpened] =
+    useState(false);
+
+  useEffect(() => {
+    const disclaimerSeenStatus: string | null =
+      localStorage.getItem("disclaimer-seen");
+    const savedCookieConsent = localStorage.getItem(COOKIE_CONSENT_STORAGE_KEY);
+
+    if (disclaimerSeenStatus === "true" && savedCookieConsent === null) {
+      setCookieConsentBannerIsOpened(true);
+    }
+  }, [disclaimerModalState]);
+
+  const handleLoadAnalyticsTags = () => {
+    loadGoogleAnalytics();
+    console.log("Google Analytics tags have been appended.");
+
+    if (cookieConsentBannerIsOpened === true) {
+      setCookieConsentBannerIsOpened(false);
+    }
+  };
+
+  return (
+    <CookieConsent
+      mode="banner"
+      handlerFunctions={[
+        { category: "analytics", function: handleLoadAnalyticsTags },
+      ]}
+      customColors={{ primary: "#3f6d3b" }}
+      language="pl"
+      customStorageKey={COOKIE_CONSENT_STORAGE_KEY}
+      modalIsOpen={cookieConsentBannerIsOpened}
+    />
+  );
+}
--- sprawdzFodmap/src/Pages/Home.tsx
+++ sprawdzFodmap/src/Pages/Home.tsx
@@ -9,12 +9,13 @@ import {
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import "./Home.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import products from "../../public/db/dietDb.ts";
 import DietCard from "../Components/Main/DietCard.tsx";
 import Navbar from "../Components/Nav.tsx";
 
 import doggie_animation from "../../public/animations/doggie 1.gif";
+import CookieConsentBanner from "../Components/CookieConsentBanner.tsx";
 
 export type ProductProp = {
   name: string;
@@ -94,92 +95,94 @@ export default function App() {
   });
 
   return (
-    <div className="main-page">
-      <Modal open={openModal} aria-labelledby="" aria-describedby="">
-        <Box className="disclaimer-modal">
-          <img id="modal-doggie" src={doggie_animation} />
-          <h2 id="disclaimer-modal-title" className="header">
-            Dobrze Cię widzieć!
-          </h2>
-          <div id="disclaimer-modal-request-div">
-            <span id="disclaimer-modal-request">
-              Zanim przejdziesz dalej, proszę, zapoznaj się z poniższym
-              zastrzeżeniem:
-            </span>
-          </div>
-          <div id="disclaimer-modal-description">
-            <p>
-              Niniejsza strona zawiera informacje na temat stosowania diet,
-              oparte na aktualnych źródłach naukowych i przeznaczona jest celom
-              edukacyjnym. Nie jest jednak w stanie zastąpić konsultacji z
-              profesjonalistą.
-            </p>
-            <p>
-              Wszelkie kwestia diagnostyczne oraz decyzje dotyczące stosowania
-              interwencji dietetycznych należy podejmować{" "}
-              <b>
-                jedynie zgodnie z zaleceniami lekarza i/lub dietetyka kliniczego
-              </b>
-              .
-            </p>
-          </div>
-          <FormControlLabel
-            id="modal-checkbox-label"
-            className="modal-checkbox-label"
-            sx={{
-              opacity: disclaimerCheckBoxOpacity,
-              pointerEvents: pointerEvtsModalCheckbox,
-            }}
-            control={
-              <Checkbox
-                style={{ color: "#000000" }}
-                onClick={() => setDisableModalButton(!disableModalButton)}
-              />
-            }
-            label="Potwierdzam zapoznanie się z powyższymi informacjami."
-          />
-          <Button
-            disabled={disableModalButton}
-            onClick={handleCloseModal}
-            variant="contained"
-          >
-            Idź dalej
-          </Button>
-        </Box>
-      </Modal>
-
-      <Navbar />
-
-      <Grid container spacing={2} className="home-grid">
-        <Grid xs={12} md={10} className="diet-grid">
-          <TextField
-            className="search-input"
-            label="Wprowadź nazwę produktu"
-            variant="standard"
-            onChange={searchData}
-          />
-        </Grid>
-        <Grid xs={12} className="diet-grid">
-          {showLoading && <CircularProgress id="loading" color="inherit" />}
-          {!showLoading && (
-            <div id="card-container">
-              {filteredData.map((p: ProductProp) => {
-                return (
-                  <DietCard
-                    name={p.name}
-                    sub_title={p.sub_title}
-                    fodmap={p.fodmap}
-                    max_use={p.max_use}
-                    histamine={p.histamine}
-                    notes={p.notes}
-                    key={p.id}
-                  />
-                );
-              })}
+    <React.Fragment>
+      <CookieConsentBanner disclaimerModalState={openModal} />
+      <div className="main-page">
+        <Modal open={openModal} aria-labelledby="" aria-describedby="">
+          <Box className="disclaimer-modal">
+            <img id="modal-doggie" src={doggie_animation} />
+            <h2 id="disclaimer-modal-title" className="header">
+              Dobrze Cię widzieć!
+            </h2>
+            <div id="disclaimer-modal-request-div">
+              <span id="disclaimer-modal-request">
+                Zanim przejdziesz dalej, proszę, zapoznaj się z poniższym
+                zastrzeżeniem:
+              </span>
             </div>
-          )}
+            <div id="disclaimer-modal-description">
+              <p>
+                Niniejsza strona zawiera informacje na temat stosowania diet,
+                oparte na aktualnych źródłach naukowych i przeznaczona jest
+                celom edukacyjnym. Nie jest jednak w stanie zastąpić konsultacji
+                z profesjonalistą.
+              </p>
+              <p>
+                Wszelkie kwestia diagnostyczne oraz decyzje dotyczące stosowania
+                interwencji dietetycznych należy podejmować{" "}
+                <b>
+                  jedynie zgodnie z zaleceniami lekarza i/lub dietetyka
+                  kliniczego
+                </b>
+                .
+              </p>
+            </div>
+            <FormControlLabel
+              id="modal-checkbox-label"
+              className="modal-checkbox-label"
+              sx={{
+                opacity: disclaimerCheckBoxOpacity,
+                pointerEvents: pointerEvtsModalCheckbox,
+              }}
+              control={
+                <Checkbox
+                  style={{ color: "#000000" }}
+                  onClick={() => setDisableModalButton(!disableModalButton)}
+                />
+              }
+              label="Potwierdzam zapoznanie się z powyższymi informacjami."
+            />
+            <Button
+              disabled={disableModalButton}
+              onClick={handleCloseModal}
+              variant="contained"
+            >
+              Idź dalej
+            </Button>
+          </Box>
+        </Modal>
+        <Navbar />
+        <Grid container spacing={2} className="home-grid">
+          <Grid xs={12} md={10} className="diet-grid">
+            <TextField
+              className="search-input"
+              label="Wprowadź nazwę produktu"
+              variant="standard"
+              onChange={searchData}
+            />
+          </Grid>
+          <Grid xs={12} className="diet-grid">
+            {showLoading && <CircularProgress id="loading" color="inherit" />}
+            {!showLoading && (
+              <div id="card-container">
+                {filteredData.map((p: ProductProp) => {
+                  return (
+                    <DietCard
+                      name={p.name}
+                      sub_title={p.sub_title}
+                      fodmap={p.fodmap}
+                      max_use={p.max_use}
+                      histamine={p.histamine}
+                      notes={p.notes}
+                      key={p.id}
+                    />
+                  );
+                })}
+              </div>
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </React.Fragment>
   );
 }
